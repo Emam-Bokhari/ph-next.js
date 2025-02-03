2011dd7fc8d8dff0179e02c842efb68252a54daf
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-03 15:54:13 +0600

    feat: add example of Link routing and useNavigaton for programatical routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
+"use client";
+import { useRouter } from "next/navigation";
 import { Fragment } from "react";
 
 export default function HomePage() {
+  const router = useRouter();
+  const handleNavigation = () => {
+    router.push("/dashboard");
+  };
   return (
     <Fragment>
-      <div>
-        <p className="text-xl text-red-500 ">Hello world</p>
+      <div className="text-center">
+        <p className="text-xl  my-4 ">Home Page</p>
+        <button
+          onClick={handleNavigation}
+          className="px-4 py-2 rounded-sm bg-indigo-500 hover:bg-indigo-600 text-white transition-all"
+        >
+          Dashboard
+        </button>
       </div>
     </Fragment>
   );
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
+export default function Navbar() {
+  console.log("Navigation");
+  return (
+    <Fragment>
+      <div className="py-4 bg-indigo-500 text-white">
+        <nav className=" flex justify-between max-w-screen-xl m-auto px-8">
+          <div>
+            <h3>Dreams</h3>
+          </div>
+          <ul className="flex gap-5 justify-center">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/products">Products</Link>
+            </li>
+            <li>
+              <Link prefetch={false} href="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link prefetch={false} href="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Fragment>
+  );
+}
